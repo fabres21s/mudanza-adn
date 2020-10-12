95c0d972d667b120bf3de0b8e6b60987475457b6
Fabio Andrés Sierra Jaraba - Ceiba Software <fabio.sierra@ceiba.com.co>
2020-10-12 16:43:15 -0500

    Los tests de los servicios web están cancionando

--- java-arquitectura-hexagonal/microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ java-arquitectura-hexagonal/microservicio/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -6,27 +6,6 @@ create table usuario (
  primary key (id)
 );
 
-
-insert into usuario(id, nombre, clave,  fecha_creacion) values (1,'pensive', 'curie', '2019-01-01');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (2,'furious', 'hugle',  '2019-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (3,'aryabhata', 'trusting',  '2019-08-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (4,'lichterman', 'grave',  '1985-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (5,'sharp', 'heisenberg',  '1945-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (6,'stoic', 'sammet',  '1998-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (7,'fervent', 'sinoussi',  '2020-02-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (8,'agitated', 'bell',  '2018-12-31');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (9,'hopeful', 'bartik',  '2020-09-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (10,'trusting', 'bohr', '2019-01-01');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (11,'hungry', 'yonath',  '2019-01-01');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (12,'ecstatic', 'jennings', '2019-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (13,'sharp', 'bhabha',  '2019-08-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (14,'dreamy', 'brattain',  '1985-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (15,'elated', 'bhabha',  '1945-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (16,'cranky', 'pike',  '1998-01-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (17,'stupefied', 'lovelace',  '2020-02-02');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (18,'sick', 'bell',  '2018-12-31');
-insert into usuario(id, nombre, clave,  fecha_creacion) values (19,'evil', 'lalande', '2020-09-02');
-
 create table festivo(
 	id int(11) not null auto_increment,
 	nombre varchar(100) not null,
@@ -35,6 +14,7 @@ create table festivo(
 	primary key (id),
 	CONSTRAINT uq_fecha UNIQUE (fecha)
 );
+
 
 insert into festivo (nombre, fecha) values ('Día de la raza', '2020-10-11');
 insert into festivo (nombre, fecha) values ('Todos los santos', '2020-11-02');
