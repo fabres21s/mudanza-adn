cddfa0535eed2cb577d06c147d2c17a2efe51823
Fabio Andrés Sierra Jaraba - Ceiba Software <fabio.sierra@ceiba.com.co>
2020-10-18 11:39:14 -0500

    Se borrar lo relacionado con usuario 2

--- java-arquitectura-hexagonal/microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ java-arquitectura-hexagonal/microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -1,34 +1,17 @@
 package com.ceiba.configuracion;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import com.ceiba.furgon.puerto.repositorio.RepositorioFurgon;
 import com.ceiba.furgon.servicio.ServicioActualizarFurgon;
 import com.ceiba.furgon.servicio.ServicioCrearFurgon;
 import com.ceiba.mudanza.puerto.repositorio.RepositorioMudanza;
 import com.ceiba.mudanza.servicio.ServicioCrearMudanza;
-import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
-import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
-import com.ceiba.usuario.servicio.ServicioCrearUsuario;
-import com.ceiba.usuario.servicio.ServicioEliminarUsuario;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
 
-    @Bean
-    public ServicioCrearUsuario servicioCrearUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioCrearUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioEliminarUsuario servicioEliminarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioEliminarUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioActualizarUsuario servicioActualizarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioActualizarUsuario(repositorioUsuario);
-    }
     
     @Bean
     public ServicioCrearFurgon servicioCrearFurgon(RepositorioFurgon repositorioFurgon) {
