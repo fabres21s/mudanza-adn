57ea2f4f46861687065df30997c5c02444308f61
Fabio Andrés Sierra Jaraba - Ceiba Software <fabio.sierra@ceiba.com.co>
2020-10-18 14:28:10 -0500

    Se agrega caso de prueba

--- java-arquitectura-hexagonal/microservicio/dominio/src/test/java/com/ceiba/furgon/servicio/ServicioActualizarFurgonTest.java
+++ java-arquitectura-hexagonal/microservicio/dominio/src/test/java/com/ceiba/furgon/servicio/ServicioActualizarFurgonTest.java
@@ -1,5 +1,7 @@
 package com.ceiba.furgon.servicio;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -10,15 +12,27 @@ import com.ceiba.furgon.puerto.repositorio.RepositorioFurgon;
 import com.ceiba.furgon.servicio.testdatabuilder.FurgonTestDataBuilder;
 
 public class ServicioActualizarFurgonTest {
+
+	@Test
+	public void validarFurgonExistenciaPreviaTest() {
+		// arrange
+		Furgon furgon = new FurgonTestDataBuilder().conId(1L).build();
+		RepositorioFurgon repositorioFurgon = Mockito.mock(RepositorioFurgon.class);
+		Mockito.when(repositorioFurgon.existeExcluyendoId(Mockito.anyLong(), Mockito.anyString())).thenReturn(true);
+		ServicioActualizarFurgon servicio = new ServicioActualizarFurgon(repositorioFurgon);
+		// act - assert
+		BasePrueba.assertThrows(() -> servicio.ejecutar(furgon), ExcepcionDuplicidad.class,
+				"La placa ingresada ya existe en el sistema");
+	}
 	
-	 @Test
-	    public void validarUsuarioExistenciaPreviaTest() {
-	        // arrange
-	        Furgon furgon = new FurgonTestDataBuilder().conId(1L).build();
-	        RepositorioFurgon repositorioFurgon = Mockito.mock(RepositorioFurgon.class);
-	        Mockito.when(repositorioFurgon.existeExcluyendoId(Mockito.anyLong(),Mockito.anyString())).thenReturn(true);
-	        ServicioActualizarFurgon servicio = new ServicioActualizarFurgon(repositorioFurgon);
-	        // act - assert
-	        BasePrueba.assertThrows(() -> servicio.ejecutar(furgon), ExcepcionDuplicidad.class,"La placa ingresada ya existe en el sistema");
-	    }
+	@Test
+	public void validarFurgonNoExistenciaPreviaTest() {
+		// arrange
+		Furgon furgon = new FurgonTestDataBuilder().conId(1L).build();
+		RepositorioFurgon repositorioFurgon = Mockito.mock(RepositorioFurgon.class);
+		Mockito.when(repositorioFurgon.existeExcluyendoId(Mockito.anyLong(), Mockito.anyString())).thenReturn(false);
+		ServicioActualizarFurgon servicio = new ServicioActualizarFurgon(repositorioFurgon);
+		// act - assert
+		 assertDoesNotThrow(()  -> servicio.ejecutar(furgon));
+	}
 }
