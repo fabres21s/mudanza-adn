5f803e83959af784adbf891de298ca342f538c30
Fabio Andrés Sierra Jaraba - Ceiba Software <fabio.sierra@ceiba.com.co>
2020-10-18 11:40:43 -0500

    Se borrar lo relacionado con usuario 3

--- java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/furgon/servicio/testdatabuilder/ComandoFurgonTestDataBuilder.java
+++ java-arquitectura-hexagonal/microservicio/infraestructura/src/test/java/com/ceiba/furgon/servicio/testdatabuilder/ComandoFurgonTestDataBuilder.java
@@ -1,11 +1,6 @@
 package com.ceiba.furgon.servicio.testdatabuilder;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 import com.ceiba.furgon.comando.ComandoFurgon;
-import com.ceiba.usuario.comando.ComandoUsuario;
-import com.ceiba.usuario.servicio.testdatabuilder.ComandoUsuarioTestDataBuilder;
 
 public class ComandoFurgonTestDataBuilder {
 
